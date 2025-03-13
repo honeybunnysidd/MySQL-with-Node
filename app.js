c41fab986828d0e9549e19e431e1cf949deec4e9
honeybunnysidd <siddhartharaghuvanshi01@gmail.com>
2025-03-13 07:33:00 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "myapp",
+  password: "Siddhartha123@",
+});
+
+// A simple SELECT query
+
+try {
+  connection.query("SHOW TABLES", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+});
+} catch (err) {
+    console.log(err);
+}
+connection.end();
+
+let createRandomUser = () => {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+};
